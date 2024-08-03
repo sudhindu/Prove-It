55e2ddfbd308a1d1684832ef1f3601fdac396bbe
Warren Craft <41027033+wdcraft01@users.noreply.github.com>
2024-08-02 19:46:32 -0600

    Update descriptive/explanatory comments in linear_algebra/matrices/identity.py
    Update descriptive/explanatory comments in linear_algebra/matrices/identity.py and provide some trivial reformatting for line length standards.

--- packages/proveit/linear_algebra/matrices/identity.py
+++ packages/proveit/linear_algebra/matrices/identity.py
@@ -2,20 +2,25 @@ from proveit import Function, Literal
 
 class Identity(Function):
     '''
-    The identity map of a vector space is a linear map that maps
-    any element to itself.
+    Represents the n x n identity matrix associated with the identity
+    map of a vector space. The identity map is a linear map that maps
+    any any element of a vector space to itself.
     '''
 
-    _operator_ = Literal(string_format='1',latex_format=r'\mathbbm{1}', theory=__file__)
+    _operator_ = Literal(string_format='1',
+            latex_format=r'\mathbbm{1}', theory=__file__)
 
     def _config_latex_tool(self, lt):
         if 'bbm' not in lt.packages:
            lt.packages.append('bbm')
 
-    def __init__(self,size, *, styles=None):
+    def __init__(self, size, *, styles=None):
         '''
-        Denote the set of linear maps that map from and to the given
-        vectors spaces.
+        Initialize a representation of a (size x size) identity matrix
+        associated with the identity map on a vector space.
+        For example, the 3 x 3 identity matrix on R^3 could be
+        intialized with Identity(num(3)).
+
         '''
         Function.__init__(self, Identity._operator_, size,
                           styles=styles)
